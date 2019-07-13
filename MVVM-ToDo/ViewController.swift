37fb8a0e24ca3408e5da301d4addbc17e42259cf
FilipGrudzinski <filip.grudzinski@b-mind.pl>
2019-07-13 12:34:02 +0200

    Initial Commit

---
+++ MVVM-ToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVM-ToDo
+//
+//  Created by Filip on 13/07/2019.
+//  Copyright © 2019 Filip. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
